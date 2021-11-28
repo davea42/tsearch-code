5bc72e1df5a2004f5ca54c5663003b6217b5e467
David Anderson <davea42@linuxmail.org>
2021-11-28 10:05:29 -0800

    deleted:    scripts/CLEANUP Removed ref to CLEANUP 	modified:   test/runtests.sh

--- test/runtests.sh
+++ test/runtests.sh
@@ -10,6 +10,24 @@ rmtemps() {
 rmtemps
 stsecs=`date '+%s'`
 
+top_blddir=`pwd`/..
+if [ x$DWTOPSRCDIR = "x" ]
+then
+  top_srcdir=$top_blddir
+else
+  top_srcdir=$DWTOPSRCDIR
+fi
+srcdir=$top_srcdir/test
+loc=`pwd`
+ct=0
+if [ ! -x $top_blddir/src/readelfobj ]
+then
+   echo "Build readelfobj and install it as $loc/../src/readelfobj to test"
+   echo "No test done"
+   exit 1
+fi
+
+
 echo "Start tsearch tests"
 date 
 x=`pwd` 
@@ -24,22 +42,15 @@ make
 cd $x
 pwd 
 echo 
-# use --std to run basic correctness tests.
-#for opts in "--std" "testwords" "sortedwords"
 for opts in "--std" "testwords" "sortedwords"
 do
-  for app  in  ../src/binarysearch ../src/eppingerdel ../src/balancedsearch ../src/redblacksearch ../src/hashsearch ../src/libcsearch 
+  for app  in  $top_blddir/src/binarysearch $top_blddir/src/eppingerdel \
+    $top_blddir/src/balancedsearch $top_blddir/src/redblacksearch \
+    $top_blddir/src/hashsearch $top_blddir/src/libcsearch 
   do
     name=`basename  $app`
     echo "==== $app $opts ===="
     /usr/bin/time $app $opts  
   done
 done
-cd ..
-sh scripts/CLEANUP
-if [ $? -ne 0 ]
-then
-  echo "file cleanup failed!"
-  exit 1
-fi
 exit 0
